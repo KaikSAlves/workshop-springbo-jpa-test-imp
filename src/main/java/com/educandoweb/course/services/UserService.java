b24176f1ccaca75365f070c9cb845c605cc0525b
KaikSAlves <souzaalveskaik@gmail.com>
2025-01-20 12:41:49 -0300

    User insert

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.User;
@@ -22,5 +23,9 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> obj = this.userRepository.findById(id);
 		return obj.get();
+	}
+	
+	public User insert(User obj) {
+		return userRepository.save(obj);
 	}
 }
