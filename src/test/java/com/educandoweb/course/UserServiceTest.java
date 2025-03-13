1deb6f2ff04b3db291cc9d570c5a3c0a99145b13
KaikSAlves <souzaalveskaik@gmail.com>
2025-03-13 17:10:07 -0300

    TESTES DE EXCESSOES - UserService.findById(int)

--- src/test/java/com/educandoweb/course/UserServiceTest.java
+++ src/test/java/com/educandoweb/course/UserServiceTest.java
@@ -1,12 +1,31 @@
 package com.educandoweb.course;
 
 import com.educandoweb.course.services.UserService;
+import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestComponent;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 
 @SpringBootTest
 public class UserServiceTest {
 
     @Autowired
     private UserService userService;
+
+
+    //TESTE DE EXCESSAO - USER SERVICE    
+    @Test
+    public void deveLancarResourceNotFoundException(){
+        assertThrows(ResourceNotFoundException.class, () -> userService.findById(10000000L));
+    }
+
+    @Test
+    public void deveLancarInvalidDataAccessApiUsageException(){
+        assertThrows(InvalidDataAccessApiUsageException.class, () -> userService.findById(null));
+    }
 }
